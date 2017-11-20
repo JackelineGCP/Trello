78e7192334d6dbe89807436ea6d97c627f23bc3f
JackelineGCP <gessenia_17@hotmail.com>
2017-11-20 00:37:02 -0500

    agrega avance de la version 1

--- js/app.js
+++ js/app.js
@@ -3,15 +3,26 @@ window.addEventListener('load',function(){
   var btn = document.getElementById("boton");
   var f1 = document.getElementById("form1");
   var f2 = document.getElementById("form2");
+  var btn2 = document.getElementById("boton2");
+
+  var t1 = document.getElementById("texto");
+  var c1 = document.getElementById("click");
+  
 
   f1.setAttribute('style','visibility:visible');
   f2.setAttribute('style','visibility:hidden');
+  c1.setAttribute('style','visibility:hidden'); 
+  
 
   btn.addEventListener("click",function(){
-
     f1.setAttribute('style','visibility:hidden');
     f2.setAttribute('style','visibility:visible');
+  });
 
+  btn2.addEventListener("click",function(){
+    t1.disabled = true;  
+    btn2.setAttribute('style','visibility:hidden');
+    c1.setAttribute('style','visibility:visible');   
   });
 
 
